a452a39326206545097993c353e30eca7aaf67fd
Pratik Jyotirling Bagale <pratikbagale8@gmail.com>
2025-06-24 22:08:43 +0530

    Add Code

---
+++ Covid/analytics/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Dashboard from "./components/dashboard/Dashboard";
+
+function App() {
+ 
+  return (
+    <>
+    
+    < Dashboard />
+    </>
+      
+  )
+}
+
+export default App
